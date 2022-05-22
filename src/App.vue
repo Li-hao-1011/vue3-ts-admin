fea4a0827d7e99d749531d4e62bf7908e9231dd5
Li Hao <1746058596@qq.com>
2022-05-22 16:03:57 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,32 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+
+
+
+
+
+
+
+
+
+    HelloWorld
+
+
+
+
+
+
+
+
+
+  }
+})
 </script>
 
 <style lang="less">
